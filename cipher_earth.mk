82f3dbeae5ea11b34472dd61648f98ceec24efcf
DtyaaAjh <100247109+FreakyFriday12@users.noreply.github.com>
2024-09-30 16:30:46 +0800

    Update cipher_earth.mk

--- cipher_earth.mk
+++ cipher_earth.mk
@@ -14,7 +14,7 @@ $(call inherit-product, device/xiaomi/earth/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := cipher_earth
+PRODUCT_NAME := derp_earth
 PRODUCT_DEVICE := earth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
@@ -26,12 +26,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="earth-user 14 UP1A.230105.007 V816.0.2.0.UCVMIXM release-keys"
 
 BUILD_FINGERPRINT := Redmi/earth_global/earth:14/UP1A.231005.007/V816.0.2.0.UCVMIXM:user/release-keys
-
-# fes anlok
-TARGET_FACE_UNLOCK_SUPPORTED := true
-
-# memtener
-CIPHER_MAINTAINER := Ditya
 
 # Time
 LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
