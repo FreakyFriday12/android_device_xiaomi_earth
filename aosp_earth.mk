c416ae58f34059c043baa450ce519532a1a6a4f9
DtyaaAjh <100247109+FreakyFriday12@users.noreply.github.com>
2024-10-11 15:00:47 +0800

    Update and rename yaap_earth.mk to aosp_earth.mk

--- aosp_earth.mk
+++ aosp_earth.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/earth/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+$(call inherit-product, vendor/Project-Elixir/config/common_full_phone.mk)
 
 PRODUCT_NAME := cipher_earth
 PRODUCT_DEVICE := earth
